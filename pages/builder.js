3cc85aa4e9a80b5d5f2a5e2bec907ddbb79a40dd
TrpFrog <dev@trpfrog.net>
2022-04-03 16:31:59 +0900

    Refactor code

--- pages/builder.js
+++ pages/builder.js
@@ -5,12 +5,13 @@ function init() {
 
 function shortenTopic(topic) {
     const maxLength = 140;
-    if(topic.length <= maxLength) return topic;
-    let html = topic.slice(0, maxLength) + '...';
-    html += '<details><summary>全文を表示</summary>'
-    html += topic
-    html += '</details>';
-    return html;
+    return topic.length <= maxLength ? topic : `
+        ${topic.slice(0, maxLength)}...
+        <details>
+            <summary>全文を表示</summary>
+            ${topic}
+        </details>
+    `;
 }
 
 function writeChannels(doSearch, searchWord) {
@@ -43,58 +44,67 @@ function writeChannels(doSearch, searchWord) {
             category.name = '<span style="font-style: italic">カテゴリ未分類</span>';
         }
 
-        html += '<details open>';
-        html += `<summary class="category">${category.name} (${channelsCount})</summary>`;
-        for (const channel of category.channels) {
-            if (doSearch && channel.name.indexOf(searchWord) === -1) {
-                continue;
-            }
-            html += '<div class="channel">';
-            html += '<span class="channel-name"> <span style="color: gray">＃</span> ' + channel.name + '</span>';
-            if (channel.hasTopic) {
-                html += '<br><span class="channel-topic">' + shortenTopic(channel.topic) + '</span>';
-            }
-            html += '</div>';
-        }
-        html += '</details>';
+        html += `
+            <details open>
+                <summary class="category">
+                    ${category.name} (${channelsCount})
+                </summary>
+                ${category
+                    .channels
+                    .filter(channel => !(doSearch && !channel.name.includes(searchWord)))
+                    .map(channel => (`
+                        <div class="channel">
+                            <span class="channel-name">
+                                <span style="color: gray">＃</span>
+                                ${channel.name}
+                            </span>
+                            ${channel.hasTopic ? `
+                                <br>
+                                <span class="channel-topic">
+                                    ${shortenTopic(channel.topic)}
+                                </span>
+                            ` : ''}
+                        </div>
+                    `)).join('')
+                }
+            </details>
+        `;
     }
     document.getElementById('content').innerHTML = html;
 }
 
 function writeLatestChannels() {
-    let html = '';
-    let channelsCount = 0;
-    for (const channel of audit) {
-        channelsCount++;
-        if (channelsCount >= 4) {
-            if (channelsCount === 4) {
-                html += '<details>'
-                html += '<summary>クリックでさらに表示</summary>'
-            }
-        }
+    const channelHTMLs = audit.map(channel => (`
+        <div class="channel latest-channel">
+            <span class="channel-name">
+                <span style="color: gray">
+                    ${channel.type === 'text' ? '＃' : 'Category: '}
+                </span>
+                ${channel.name}
+            </span>
+            ${channel.hasTopic ? `
+                <br>
+                <span class="channel-topic">
+                    ${shortenTopic(channel.topic)}
+                </span>
+            ` : ''}
+            <hr>
+            <span class="channel-author">
+                ${channel.hasParentCategory ? `Category: ${channel.parentCategory}<br>` : ''}
+                ${channel.creator} (${channel.createdAt})
+            </span>
+        </div>
+    `))
 
-        html += '<div class="channel latest-channel">';
-        const icon = channel.type === 'text' ? '＃' : 'Category: ';
-        html += '<span class="channel-name"> <span style="color: gray">' + icon + '</span> '
-        html += channel.name + '</span>';
-        if (channel.hasTopic) {
-            html += '<br><span class="channel-topic">' + shortenTopic(channel.topic) + '</span>';
-        }
-
-        html += '<hr>';
-        html += '<span class="channel-author">';
-        if (channel.hasParentCategory) {
-            html += 'Category: ' + channel.parentCategory + '<br>';
-        }
-        html += channel.creator + ' ';
-        html += '(' + channel.createdAt + ')';
-        html += '</span>';
-        html += '</div>';
-    }
-    if(channelsCount > 3) {
-        html += '</details>'
-    }
-    document.getElementById('latest').innerHTML = html;
+    document.getElementById('latest').innerHTML = `
+        ${channelHTMLs.slice(0, 3).join('')}
+        ${channelHTMLs.length >= 4 ? `
+            <details>
+                <summary>クリックでさらに表示</summary>
+                ${channelHTMLs.slice(3).join('')}
+            </details>
+        ` : ''}
+    `;
 }
 
 function searchChannels() {
